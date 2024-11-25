e0df68e3324e33d219545a79ea8e7a9089f186ac
BALAJI K <balajik.0204@gmail.com>
2024-11-25 12:39:18 +0530

    updated header component

--- video-twitch/components/stream-player/index.tsx
+++ video-twitch/components/stream-player/index.tsx
@@ -9,6 +9,8 @@ import { cn } from "@/lib/utils";
 import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
+import { Header, HeaderSkeleton } from "./header";
+import { ChatHeaderSkeleton } from "./chat-header";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null }; 
@@ -52,6 +54,15 @@ export const StreamPlayer = ({
             hostName={user.username}
             hostIdentity={user.id}
           />
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
+
         </div>
         <div
         className={cn(
@@ -84,7 +95,7 @@ export const StreamPlayerSkeleton = () => {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
-        {/* TODO: Header Skeleton */}
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg-background">
         <ChatSkeleton />
